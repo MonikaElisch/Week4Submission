ef566822d5c35dc06d5510ce8bb924fa15f4af44
MonikaElisch <125794798+MonikaElisch@users.noreply.github.com>
2024-01-30 12:21:21 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class MyHomePage extends StatelessWidget {
                 name: "Pixel",
                 description: "Pixel is the most featureful phone ever",
                 price: 800,
-                image: "apple.png"),
+                image: "Apple.png"),
             ProductBox(
                 name: "Laptop",
                 description: "Laptop is most productive development tool",
